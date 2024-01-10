c208c5911530934af58ef39fee245b73a5145cca
Miguel Ángel Manzano <mmanzanomo@gmail.com>
2024-01-10 22:46:57 +0100

    Added EventsConsumer and EventsProducer

--- src/main/java/com/mmanzanomo/rabbitmq/EventsConsumer.java
+++ src/main/java/com/mmanzanomo/rabbitmq/EventsConsumer.java
@@ -1,4 +1,34 @@
 package com.mmanzanomo.rabbitmq;
 
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.concurrent.TimeoutException;
+
 public class EventsConsumer {
+    private static final String EXCHANGE = "events";
+    public static void main(String[] args) throws IOException, TimeoutException {
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        Connection connection = connectionFactory.newConnection();
+
+        Channel channel = connection.createChannel();
+        channel.exchangeDeclare(EXCHANGE, BuiltinExchangeType.FANOUT);
+        String queueName = channel.queueDeclare().getQueue();
+        channel.queueBind(queueName, EXCHANGE, "");
+        // Create subscrition to queue using
+        channel.basicConsume(queueName, true,
+                (consumerTag, message) -> {
+                    String messageBody = new String(message.getBody(), Charset.defaultCharset());
+
+                    System.out.println("Message: " + messageBody);
+                },
+                consumerTag -> {
+                    System.out.println("Consumer: " + consumerTag + " cancelled.");
+                });
+    }
 }
--- src/main/java/com/mmanzanomo/rabbitmq/EventsProducer.java
+++ src/main/java/com/mmanzanomo/rabbitmq/EventsProducer.java
@@ -1,4 +1,31 @@
 package com.mmanzanomo.rabbitmq;
 
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class EventsProducer {
+    private static final String EXCHANGE = "events";
+
+    public static void main(String[] args) throws IOException, TimeoutException, InterruptedException {
+
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+
+        try(Connection connection = connectionFactory.newConnection();
+            Channel channel = connection.createChannel()) {
+            channel.exchangeDeclare(EXCHANGE, BuiltinExchangeType.FANOUT);
+
+            int count = 0;
+            while(true) {
+                String message = "Event n: " + count++;
+                channel.basicPublish(EXCHANGE, "", null, message.getBytes());
+                Thread.sleep(1000);
+            }
+        }
+
+    }
 }
